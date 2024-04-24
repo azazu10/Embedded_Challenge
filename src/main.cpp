9e971dc5a8259ddfa95cc706d7426dd7fc82fb10
ZARAEM <mearaz.ahmed@nyu.edu>
2024-04-24 11:48:30 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,73 @@
-#include <Arduino.h>
-#include <arduinoFFT.h>
 #include <Adafruit_CircuitPlayground.h>
+//#define FFT_SPEED_OVER_PRECISION /* faster but prone to precision errors */
+//#define USE_AVR_PROGMEM /* stores some stuff in memory ? */
+#include <arduinoFFT.h>
 
 float X, Y, Z;
-float acc;
+
+const uint16_t samples = 64; //This value MUST ALWAYS be a power of 2
+const float signalFrequency = 1000;
+const float samplingFrequency = 5000;
+const uint8_t amplitude = 100;
+
+float xReal[samples];
+float yReal[samples];
+float zReal[samples];
+float emptyImag[samples];
+
+ArduinoFFT<float> ThreeFFT = ArduinoFFT<float>(xReal, emptyImag, samples, samplingFrequency);
+ArduinoFFT<float> FourFFT = ArduinoFFT<float>(yReal, emptyImag, samples, samplingFrequency);
+ArduinoFFT<float> FiveFFT = ArduinoFFT<float>(zReal, emptyImag, samples, samplingFrequency);
+ArduinoFFT<float> SixFFT = ArduinoFFT<float>(zReal, emptyImag, samples, samplingFrequency);
 
 void setup() {
   Serial.begin(9600);
   CircuitPlayground.begin();
-  CircuitPlayground.setAccelRange(LIS3DH_RANGE_8_G);  // Can be 2, 4, or 8
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(" ");
 
   X = CircuitPlayground.motionX();
-  Serial.print("\nAcceleration in X: ");
-  Serial.print(X);
-
   Y = CircuitPlayground.motionY();
-  Serial.print("\nAcceleration in Y: ");
-  Serial.print(Y);
-
   Z = CircuitPlayground.motionZ();
-  Serial.print("\nAcceleration in Z: ");
-  Serial.print(Z);
 
-  acc = sqrt((X*X)+(Y*Y)+(Z*Z));
-  Serial.print("\nAcceleration: ");
-  Serial.print(acc);
-  
-  delay(1000);
+  Serial.print("X: ");
+  Serial.print(X);
+  Serial.print("  Y: ");
+  Serial.print(Y);
+  Serial.print("  Z: ");
+  Serial.println(Z);
+
+  //acc = sqrt((X*X)+(Y*Y)+(Z*Z));
+  //Serial.print("\nAcceleration: ");
+  //Serial.print(acc);
+
+  ThreeFFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  FourFFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  FiveFFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  SixFFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+
+  ThreeFFT.compute(FFTDirection::Forward);
+  FourFFT.compute(FFTDirection::Forward);
+  FiveFFT.compute(FFTDirection::Forward);
+  SixFFT.compute(FFTDirection::Forward);
+
+  ThreeFFT.complexToMagnitude();
+  FourFFT.complexToMagnitude();
+  FiveFFT.complexToMagnitude();
+  SixFFT.complexToMagnitude();
+
+  float threeHz = ThreeFFT.majorPeak();
+  float fourHz = FourFFT.majorPeak();
+  float fiveHz = FiveFFT.majorPeak();
+  float sixHz = SixFFT.majorPeak();
+
+  Serial.print("3Hz: ");
+  Serial.print(threeHz);
+  Serial.print("4Hz: ");
+  Serial.print(fourHz);
+  Serial.print("5Hz: ");
+  Serial.print(fiveHz);
+  Serial.print("6Hz: ");
+  Serial.println(sixHz);
 }
