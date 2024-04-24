6a2af9d6526e462c61efc793bb35fd11e278f170
azazu10 <an3525@nyu.edu>
2024-04-24 11:21:31 -0400

    Getting Average Acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_CircuitPlayground.h>
 
 float X, Y, Z;
+float acc;
 
 void setup() {
   Serial.begin(9600);
@@ -12,17 +13,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(" ");
+
   X = CircuitPlayground.motionX();
-  Serial.print("Acceleration in X: ");
+  Serial.print("\nAcceleration in X: ");
   Serial.print(X);
+
   Y = CircuitPlayground.motionY();
   Serial.print("\nAcceleration in Y: ");
   Serial.print(Y);
+
   Z = CircuitPlayground.motionZ();
   Serial.print("\nAcceleration in Z: ");
-  Serial.println(Z);
-  delay(1000);
+  Serial.print(Z);
 
-  int Mat [][3] = {{},
-              };
+  acc = sqrt((X*X)+(Y*Y)+(Z*Z));
+  Serial.print("\nAcceleration: ");
+  Serial.print(acc);
+  
+  delay(1000);
 }
